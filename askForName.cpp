a079c688ab58b88d84d640f29d61719a1b7d3d6a
Spencer Sansone <spencersemail101@gmail.com>
2018-07-17 00:17:49 +0000

    improve askForName.cpp

--- askForName.cpp
+++ askForName.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main()
 {
-  std::string name;
-  std::cout << "What is your name? ";
-  getline (std::cin, name);
-  std::cout << "Hello, " << name << "!\n";
+  string name;
+  cout << "What is your name? ";
+  cin >> name;
+  cout << "Hello, " << name << "!\n";
 }
